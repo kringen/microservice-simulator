017bc2ad5a88b5daacf5a7d4cbd50987d859dd58
Erik Kringen <ekringen@gmail.com>
2024-03-08 20:22:06 -0500

    Updating logging

--- generator/main.go
+++ generator/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log/slog"
 	"os"
-	"time"
 
 	"github.com/google/uuid"
+
 	rmq "github.com/kringen/message-center/rabbitmq"
 )
 
@@ -15,62 +14,49 @@ var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 func main() {
 
+	messageCenter := rmq.MessageCenter{}
+
+	employeeSaga := rmq.Saga{
+		SagaName:        "Create Employee",
+		SagaDescription: "Saga to create an employee using the employee service",
+		ChannelName:     "employee",
+		CorrelationId:   uuid.New().String(),
+	}
+
 	// Connect to the channel
 	// Establish messaging connection
-	messageCenter := rmq.MessageCenter{}
-	// Define RabbitMQ server URL.
+
 	messageCenter.ServerUrl = os.Getenv("RABBIT_URL")
-	channelName := "employee"
-	queueName := "employee_create"
-	err := messageCenter.Connect(channelName, 5, 5)
+	err := messageCenter.Connect("employee", 5, 5)
 	if err != nil {
 		panic(err)
 	}
 	defer messageCenter.Connection.Close()
 	defer messageCenter.Channel.Close()
 
-	logger.Info("Creating queue for employee reply...")
-	queueId := uuid.New()
+	stepOne := rmq.SagaStep{
+		StepName:        "Publish Create Employee",
+		StepDescription: "Send create request to Employee service",
+		QueueName:       "employee_create",
+		ActionType:      "publish_and_confirm",
+		ReplyQueueName:  uuid.New().String(),
+	}
+	logger.Info("Creating Saga step one")
+	employeeSaga.SagaSteps = append(employeeSaga.SagaSteps, stepOne)
 
-	err = messageCenter.CreateQueue(queueId.String(), false, false, false, false, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	// Create a test read message
-	correlationId := uuid.New()
-	employeeGetRequest := EmployeeGetRequest{
-		CorrelationId: correlationId.String(),
-		ReplyQueue:    queueId.String(),
-		EmpId:         "1",
-	}
-	egr, err := json.Marshal(employeeGetRequest)
-	if err != nil {
-		panic(err)
-	}
-	err = messageCenter.PublishMessage(queueName, egr, "", false, false, "text/plain")
-	if err != nil {
-		panic(err)
-	}
+	/////////////////////////
+	// TEST
 	// Create a test create message
 	employee := Employee{
-		FirstName: "Jason",
-		LastName:  "Bourne",
+		CorrelationId: employeeSaga.CorrelationId,
+		FirstName:     "Jason",
+		LastName:      "Bourne",
 	}
-	b, err := json.Marshal(employee)
-	if err != nil {
-		panic(err)
-	}
-	interval := time.Duration(1) * time.Second
-	attempts := 500
-	time.Sleep(120 * time.Second) // Initial wait period
-	for i := 0; i < attempts; i++ {
-		err = messageCenter.PublishMessage(queueName, b, "", false, false, "text/plain")
-		if err != nil {
-			panic(err)
-		}
-		logger.Info(fmt.Sprintf("Publishing message %d", i))
-		time.Sleep(interval)
-	}
+
+	// Serialize to json
+	b, _ := json.Marshal(employee)
+	stepOne.DataObject = b
+	logger.Info("Starting Saga")
+	employeeSaga.StartSaga(&messageCenter)
 
 }
